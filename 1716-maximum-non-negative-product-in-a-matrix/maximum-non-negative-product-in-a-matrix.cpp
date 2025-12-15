d36e2b2a24ab50309173b0761ae61d62c27cd796
Shivansh Gupta <122895124+shivaaansh@users.noreply.github.com>
2025-12-15 22:12:22 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (48.84%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
@@ -38,17 +38,34 @@ public:
         return {mx, mn};
     }
 
-    int maxProductPath(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
+        int maxProductPath(vector<vector<int>>& grid) {
+        int m=grid.size(), n=grid[0].size(), MOD = 1e9+7;
+        // we use long long to avoid overflow
+        vector<vector<long long>>mx(m,vector<long long>(n)), mn(m,vector<long long>(n));
+        mx[0][0]=mn[0][0]=grid[0][0];
+        
+        // initialize the top and left sides
+        for(int i=1; i<m; i++){
+            mn[i][0] = mx[i][0] = mx[i-1][0] * grid[i][0];
+        }
+        for(int j=1; j<n; j++){
+            mn[0][j] = mx[0][j] = mx[0][j-1] * grid[0][j];
+        }
 
-        dpMax.assign(n, vector<long long>(m));
-        dpMin.assign(n, vector<long long>(m));
-        vis.assign(n, vector<bool>(m, false));
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(grid[i][j] < 0) { // minimum product * negative number = new maximum product
+                    mx[i][j] = min(mn[i-1][j], mn[i][j-1]) * grid[i][j];
+                    mn[i][j] = max(mx[i-1][j], mx[i][j-1]) * grid[i][j];
+                }
+                else { // maximum product * positive number = new maximum product
+                    mx[i][j] = max(mx[i-1][j], mx[i][j-1]) * grid[i][j];
+                    mn[i][j] = min(mn[i-1][j], mn[i][j-1]) * grid[i][j];
+                }
+            }
+        }
 
-        auto res = dfs(n - 1, m - 1, grid);
-
-        if (res.first < 0) return -1;
-        return res.first % MOD;
+        int ans = mx[m-1][n-1] % MOD;
+        return ans < 0 ? -1 : ans;
     }
 };
