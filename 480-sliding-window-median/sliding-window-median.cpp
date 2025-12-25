c66ddda6944260dfb8bc7150ed5b5954088796ff
Shivansh Gupta <122895124+shivaaansh@users.noreply.github.com>
2025-12-25 20:31:53 +0530

    Time: 35 ms (92.67%) | Memory: 40.1 MB (75.83%) - LeetSync

---
+++ 480-sliding-window-median/sliding-window-median.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
+        vector<double> medians;
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        priority_queue<int> maxh;
+        priority_queue<int, vector<int>, greater<int>> minh;
+        for(int i = 0; i < k; i++) {
+            maxh.push(nums[i]);
+        }
+        for(int i = 0; i < (k/2); i++) {
+            minh.push(maxh.top());
+            maxh.pop();
+        }
+        for(int i = k; i < n; i++) {
+            if(k&1) {
+                medians.push_back(maxh.top()*1.0);
+            }else {
+                medians.push_back(((double) maxh.top() + (double) minh.top())/2.0);
+            }
+            int p = nums[i - k], q = nums[i];
+            int balance = 0;
+            if(p <= maxh.top()) {
+                balance--;
+                if(p == maxh.top()) {
+                    maxh.pop();
+                }else {
+                    mp[p]++;
+                }
+            }
+            else {
+                balance++;
+                if(p == minh.top()) {
+                    minh.pop();
+                }else {
+                    mp[p]++;
+                }
+            }
+            if(!maxh.empty() && q <= maxh.top()) {
+                maxh.push(q);
+                balance++;
+            }else {
+                minh.push(q);
+                balance--;
+            }
+            if(balance < 0) {
+                maxh.push(minh.top());
+                minh.pop();
+            }
+            else if(balance > 0) {
+                minh.push(maxh.top());
+                maxh.pop();
+            }
+            while(!maxh.empty() && mp[maxh.top()]) {
+                mp[maxh.top()]--;
+                maxh.pop();
+            }
+            while(!minh.empty() && mp[minh.top()]) {
+                mp[minh.top()]--;
+                minh.pop();
+            }
+        }
+        if(k&1) {
+            medians.push_back(maxh.top()*1.0);
+        }else {
+            medians.push_back(((double) maxh.top() + (double) minh.top())/2.0);
+        }
+        return medians;
+    }
+};
